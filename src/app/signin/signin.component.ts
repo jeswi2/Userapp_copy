a83b4416640b9bfcc78a00420e28c40ab5a1ba5c
jeswi2 <jeswimathewm@gmail.com>
2022-10-15 00:09:30 +0530

    fo

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myrouter:Router) { }
+
+  username=""
+  password=""
+
+  readValues=()=>{
+    let data={
+      "username":this.username,
+      "password":this.password
+    }
+    if ( this.username=="admin" && this.password=="12345") {
+      this.myrouter.navigate(["/dashboard"])
+      } else {
+    
+        alert("invalid")
+      }
+
+    console.log(data)
+  }
 
   ngOnInit(): void {
   }
